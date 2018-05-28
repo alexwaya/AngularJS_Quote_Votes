8c97a84a911369f6da4f7a7c23ec79bc882c900c
alexwaya <alex.waya@gmail.com>
2018-05-28 16:22:30 +0300

    add fixed footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { PublishComponent } from './publish/publish.component';
+import { VoteComponent } from './vote/vote.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    PublishComponent,
+    VoteComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
